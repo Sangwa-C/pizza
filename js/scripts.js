1d1e3eda3cc998cd36db871018a9da792228dfbd
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-03 13:20:22 +0200

    working on java

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-// function order (form){
-//    this.
-// }
+// business logic
+function order (size, crust, topping, number, deliver){
+  this.size= size;
+  this.crust=crust;
+  this.topping= topping;
+  this.number=number;
+  this.deviler= deliver;
+}
+
+order.prototype.pizza=function(){
+  return this.size+ " , " + this.crust + " , " + this.topping + " , " + this.number;
+}
+
+// user interface logic
+$(document).ready(function() {
+  var inputtedSize=$("inputted#size").val();
+  var inputtedCrust=$("inputted#crust").val();
+  var inputtedtopping=$("inputted#topping").val();
+  var inputtedNUmber=$("inputted#number").val();
+  var newPizza= new order (inputtedSize,inputtedCrust,inputtedtopping,inputtedNUmber);
+$("p#sample").append("<p>" + newPizza.pizza() + "</p>");
+});
