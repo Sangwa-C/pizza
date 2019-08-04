ccb14f156c8a5ed59b9914eca720347b86aad1f9
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-04 23:04:23 +0200

    added the javascript to display the amount to be charged

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // business logic
-function Order(size, crust, topping){
+function Order(size, crust, topping, number){
   this.size= size;
   this.crust=crust;
   this.topping= topping;
+  this.number= number;
 }
 
 Order.prototype.pizza=function(){
-  return this.size+ " , " + this.crust + " , " + this.topping  
+  return "Your Order: Pizza size is " + this.size+ ", crust chosen is " + this.crust + ", with " + this.topping + ", the quantity is " + this.number
 }
 
 
@@ -19,15 +20,65 @@ $(document).ready(function() {
   $("#oda").click (function(){
 
     var inputtedSize=$("select#size").val();
-    console.log("inputtedSize");
+    // console.log("inputtedSize");
+    if ($("select#size").val()==="small"){
+      var money1 = 5
+    }else if ($("select#size").val()==="medium"){
+      var money1 = 7
+    }else if ($("select#size").val()==="large"){
+      var money1 = 12
+    } else{
+      alert("Please refresh and re-order")
+    }
+
+
     var inputtedCrust=$("select#crust").val();
-    console.log("inputtedCrust");
+    // console.log("inputtedCrust");
+    if ($("select#crust").val() ==="Crispy"){
+      var money2= 1
+    } else if ($("select#crust").val() ==="Stuffed"){
+      var money2= 3
+    }else if ($("select#crust").val() ==="Gluten-free"){
+      var money2= 5
+    }else{
+      alert("Please refresh and re-order")
+    }
+
     var inputtedTopping=$("select#topping").val();
     console.log("inputtedTopping");
+    if($("select#topping").val()==="Pepperoni"){
+      var money3=1
+    } else if ($("select#topping").val()==="Mushrooms"){
+      var money3=1
+    } else if ($("select#topping").val()==="Onions"){
+      var money3=1
+    }  else if ($("select#topping").val()==="Sausage"){
+      var money3=1
+    } else if ($("select#topping").val()==="Bacon"){
+      var money3=1
+    }  else if ($("select#topping").val()==="Extra cheese"){
+      var money3=1
+    } else if ($("select#topping").val()==="Black olives"){
+      var money3=1
+    } else if ($("select#topping").val()==="Green peppers"){
+      var money3=1
+    } else {
+      alert("Please refresh and re-order")
+    }
+    
+    var inputtedNumber=$("select#number").val();
+    console.log(inputtedNumber);
+    // if($("select#number").val()===1){
+    //   var money3=1
+    // }
+    var newPizza= new Order (inputtedSize, inputtedCrust, inputtedTopping,inputtedNumber);
+    var pizzaMoney= parseInt(money1) +parseInt(money2)+parseInt(money3)
+    // var totalMoney = pizzaMoney*("select#number")
 
-    var newPizza= new Order (inputtedSize, inputtedCrust, inputtedTopping);
+    $("div#sample").append("<p>" + "The charge will be " + pizzaMoney + " $"+ "</p>");
+    console.log(pizzaMoney);
 
-    $("ul#sample").append("<li><span>" + newPizza.pizza() + "<li><span>");
+    $("div#sample").append("<p>" + newPizza.pizza() + "</p>");
     console.log(newPizza);
     });
 
@@ -38,8 +89,8 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-  $("#delivery").click (function(){
-    
+  $("#delivery").click (function(){            
+
     $("#address").show();
     alert("your order will be delivered to your location. let us know your location!")
   });
